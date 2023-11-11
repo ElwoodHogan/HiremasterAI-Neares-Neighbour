ecdeabf60870dbf4cdbfef7b894f81e7841946c6
Jacob Moore <jacobmoore93@gmail.com>
2023-11-11 03:01:13 -0600

    It's not much but It should be enough to generate a landing page. It is not randomized.

--- HTML-Gen.py
+++ HTML-Gen.py
@@ -4,35 +4,65 @@
 import dominate
 from dominate.tags import *
 
+
 def create_page():
-    
-    #Creates a html document.
+    # Creates an html document.
     doc = dominate.document(title='War Eagle LLC')
-    
-    # Body     
+
+    # Body
     with doc:
         with div(cls='container'):
-            h1('War Eagle, LLC')
-            p('''
-              This is where the job description should go.
-              We want you to come join our team of engineers.
-              Creating software that will change lives.
-              ''')    
-            p('We belive in work. Hard work.')
-              
-        p(kbd('First Name: ', input_()))
-        p(kbd('Last Name: ', input_()))
-        p(kbd('Phone Number: ', input_()))
-        p(kbd('Email Address: ', input_()))
-        p(kbd('Zipcode: ', input_()))
-        
-        button('Apply')
-      
-    return doc.render()
-      
-html_content = create_page()        
-                                 
-with open('LP.html', 'w') as f:
-    f.write(html_content) 
+            # Split layout with flexbox
+            with div(style="display: flex; justify-content: space-between;"):
+                # Left side for job description
+                with div(style="width: 60%"):  # Adjust the width as needed
+                    h1('War Eagle, LLC')
+                    p('''
+                      This is where the job description should go.
+                      We want you to come join our team of engineers.
+                      Creating software that will change lives.
+                      ''')
+                    p('This is where the job benifits would go.')
+                    p('We believe in work. Hard work.')
 
+                # Right side for the table
+                with div(style="width: 40%"):  # Adjust the width as needed
+                    # Table for input fields
+                    with table(border=1, style="width: 45%"):
+                        with tbody():
+                            with tr():
+                                td('First Name:')
+                                td(input_(type='text', name='first_name'))
+
+                            with tr():
+                                td('Last Name:')
+                                td(input_(type='text', name='last_name'))
+
+                            with tr():
+                                td('Phone Number:')
+                                td(input_(type='tel', name='phone_number'))
+                                
+                            with tr():
+                                td('Zipcode:')
+                                td(input_(type='text', name='zipcode'))
+
+                            with tr():
+                                td('Email Address:')
+                                td(input_(type='email', name='email_address'))
+                                
+                            with tr():
+                                td('Confirm Email Address:')
+                                td(input_(type='email', name='email_address'))    
+
+                            
+
+                    button('Apply')
+
+    return doc.render()
+
+
+html_content = create_page()
+
+with open('LP.html', 'w') as f:
+    f.write(html_content)
 
