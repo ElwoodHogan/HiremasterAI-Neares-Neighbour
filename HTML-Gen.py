08850e36856238c3db6f2f898dfd396f18142805
Jacob Moore <jwm0083@auburn.edu>
2023-11-19 07:51:32 -0600

    Updated html generator.

--- HTML-Gen.py
+++ HTML-Gen.py
@@ -4,64 +4,73 @@
 import dominate
 from dominate.tags import *
 
+# TODO create RGB variables and use it to concatinate 
+# have css values as global variables. 
+# go with non continuos values aka use integers values not floating point.
+# try 16-bit color to try and not overwhelm knn
+# 
 
 
 def create_page():
     # Creates an html document.
-    doc = dominate.document(title='War Eagle LLC')
+    doc = dominate.document(title='War Eagle LLC', style='background: navy')
 
     # Body
     with doc:
-        with div(cls='container'):
-            # Split layout with flexbox
-            with div(style="display: flex; justify-content: space-between;"):
-                # Left side for job description
-                with div(style="width: 60%"):
-                    h1('War Eagle, LLC')
-                    p('''
-                      This is where the job description should go.
-                      We want you to come join our team of engineers.
-                      Creating software that will change lives.
-                      ''')
-                    p('This is where the job benifits would go.')
-                    p('We believe in work. Hard work.')
+        
+        h1('War Eagle, LLC', style='color: orange; text-align: center; border: 3px solid orange')
+    
+        # Split layout with flexbox
+        with div(style="display: flex; justify-content: space-between;"):
+            # Left side for job description
+            with div(style="width: 40%"):
+                h2('Job Title: Worker', style='color: white; text-align: center')
+                p('''
+                  This is where the job description should go.
+                  We want you to come join our team of engineers.
+                  Creating software that will change lives.
+                  ''', style='color: orange')
+                p('This is where the job benifits would go.', style='color: orange')
+                p('We believe in work. Hard work.', style='color: orange')
 
-                # Right side for the table
-                with div(style="width: 40%"):
-                    # Table for input fields
-                    with table(border=1, style="width: 90%"):
-                        with tbody():
-                            with tr():
-                                td('First Name:')
-                                td(input_(type='text', name='first_name'))
+            # Right side for the table
+            with div(style="width: 50%"):
+                # Table for input fields
+                with table(style='width: 100%; background: navy; overflow-x:auto'):
+                    with tbody():
+                        with tr():
+                            td('First Name:', style='color: white')
+                            td(input_(type='text', name='first_name'))
 
-                            with tr():
-                                td('Last Name:')
-                                td(input_(type='text', name='last_name'))
+                        with tr():
+                            td('Last Name:', style='color: white')
+                            td(input_(style='padding: 4px', type='text', name='last_name'))
 
-                            with tr():
-                                td('Phone Number:')
-                                td(input_(type='tel', name='phone_number'))
-                                
-                            with tr():
-                                td('Zipcode:')
-                                td(input_(type='text', name='zipcode'))
-
-                            with tr():
-                                td('Email Address:')
-                                td(input_(type='email', name='email_address'))
-                                
-                            with tr():
-                                td('Confirm Email Address:')
-                                td(input_(type='email', name='email_address'))    
+                        with tr():
+                            td('Phone Number:', style='color: white')
+                            td(input_(style='padding: 4px', type='tel', name='phone_number'))
                             
-                            with tr():
-                                td('Job History:')
-                                td(input_(type='text', name='company'))
-                                td(input_(type='text', name='job_title'))
-                                td(input_(type='number', name='time_there'))
+                        with tr():
+                            td('Zipcode:', style='color: white')
+                            td(input_(style='padding: 4px', type='text', name='zipcode'))
 
-                    button('Apply')
+                        with tr():
+                            td('Email Address:', style='color: white')
+                            td(input_(style='padding: 4px', type='email', name='email_address'))
+                            
+                        with tr():
+                            td('Confirm Email Address:', style='color: white')
+                            td(input_(style='padding: 4px', type='email', name='email_address'))    
+                        
+                        with tr():
+                            td('Please tell us your last 3 jobs.', style='color: white')
+                        with tr():    
+                            td('Job History:', style='color: white')
+                            td(input_(style='padding: 4px', type='text', name='company'))
+                            td(input_(style='padding: 4px', type='text', name='job_title'))
+                            td(input_(style='padding: 4px', type='number', name='time_there'))
+
+                button('Apply', style='color: navy')
 
     return doc.render()
 
