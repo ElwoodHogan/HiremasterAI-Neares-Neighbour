f07d617d1de59a0442d905fc798a71a0949f1448
Jacob Moore <jacobmoore93@gmail.com>
2023-11-25 18:04:08 -0600

    Updated website. Still need to make global variabls.

--- HTML-Gen.py
+++ HTML-Gen.py
@@ -16,16 +16,17 @@ def create_page():
     doc = dominate.document(title='War Eagle LLC', style='background: navy')
     
     with doc.head:
-        p('War Eagle, LLC', style='color: orange; text-align: center; border: 3px solid orange')
+        h1('War Eagle, LLC', style='color: orange; text-align: center; border: 3px solid orange')
+        p('Talk to a recruiter today (734) 288-2827', style='text-align: center; color: white')
     # Body
     with doc:
     
         # Split layout with flexbox
         with div(style="display: flex; justify-content: space-between;"):
             # Left side for job description
-            with div(style="width: 40%"):
+            with div(style='width: 40%'):
                 h2('Job Title: Worker', style='color: white; text-align: center')
-                p(strong('Anual Salary of $75K', style='color: orange'))
+                p(strong('Anual Salary of $75K-$125K', style='color: orange'))
                 description_list = ul(style='color: orange')
                 with description_list:
                     li('War')
@@ -33,14 +34,19 @@ def create_page():
                     li('Fearless')
                     li('And')
                     li('True')
-                p(description_list)
-                p('This is where the job benifits would go.', style='color: orange')
+                
+                benifits_list = ul(style='color: white')
+                with benifits_list:
+                    li('Great Healthcare Options')
+                    li('Accrue up to 3 weeks paid vacation each year')
+                    li('Paternity leave')
+                
                 
 
             # Right side for the table
-            with div(style="width: 50%"):
+            with div(style='width: 50%; height: 600px; background-image: url(https://www.dictionary.com/e/wp-content/uploads/2018/05/war-eagle-3.jpg); background-repeat: no-repeat'):
                 # Table for input fields
-                with table(style='width: 100%; background: navy').add(tbody()):
+                with table(style='width: 100%').add(tbody()):
                     with tr():
                         th('First Name:', style='color: white')
                         td(input_(style='padding: 4px', type='text', name='first_name'))
@@ -63,16 +69,28 @@ def create_page():
                             
                     with tr():
                         th('Confirm Email Address:', style='color: white')
-                        td(input_(style='padding: 4px', type='email', name='email_address'))    
-                        
+                        td(input_(style='padding: 4px', type='email', name='email_address')) 
+                           
+                p('Please provide your previous job:', style='text-align: center; color: white')
+                
+                with table(style='width: 100%').add(tbody()):         
                     with tr():    
-                        th('Please provide the last 5 years of your job history:', style='color: white')
+                        th('Employer:', style='color: white')
                         td(input_(style='padding: 4px', type='text', name='company'))
+                        
+                    with tr():
+                        th('Job title:', style='color: white')
                         td(input_(style='padding: 4px', type='text', name='job_title'))
-                        td(input_(style='padding: 4px', type='number', name='time_there'))
+                    with tr():
+                        th('Start and end date:', style='color: white')
+                        td(input_(type='date', name='start_date'))
+                        td(input_(type='date', name='end_date'))
 
                 button('Apply', style='color: navy')
-        footer(sub('We believe in work. Hard work.', style='color: orange'))
+    
+    with doc.footer:
+        sub('We believe in work. Hard work.', style='color: orange')
+    
     return doc.render()
 
 
